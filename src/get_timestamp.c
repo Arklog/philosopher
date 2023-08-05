6228fd474aae54078032f31f63b34d9be5fecf6a
Arklog <49959852+Arklog@users.noreply.github.com>
2023-08-05 08:28:40 +0200

    restart

--- src/get_timestamp.c
+++
@@ -1,9 +0,0 @@
-#include "output.h"
-
-u_int64_t	get_timestamp(void)
-{
-	struct timeval	b;
-
-	gettimeofday(&b, NULL);
-	return (b.tv_sec * 1000 + b.tv_usec / 1000);
-}
--- src/philo_set_finished.c
+++
@@ -1,8 +0,0 @@
-#include "philo.h"
-
-void	philo_set_finished(t_philo_data *d)
-{
-	pthread_mutex_lock(&(d->mutex_is_finished));
-	d->is_finished = true;
-	pthread_mutex_unlock(&(d->mutex_is_finished));
-}
