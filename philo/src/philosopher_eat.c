17289df62568980ed730f24e9785793bcc2c7056
Pierre DUCLOUX <pducloux@paul-f3Br7s5.clusters.42paris.fr>
2023-10-11 07:17:56 +0200

    update

--- philo/src/philosopher_eat.c
+++ philo/src/philosopher_eat.c
@@ -6,7 +6,7 @@
 /*   By: pducloux <pducloux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 19:39:47 by pducloux          #+#    #+#             */
-/*   Updated: 2023/10/11 05:12:14 by pducloux         ###   ########.fr       */
+/*   Updated: 2023/10/11 07:16:59 by pducloux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ static void	eat(t_philosopher *p)
 	t_philo_data	*datas;
 
 	datas = p->datas;
-	pthread_mutex_lock(&(p->last_eat_mutex.m));
-	p->last_eat = gettimestamp();
-	pthread_mutex_unlock(&(p->last_eat_mutex.m));
+	// pthread_mutex_lock(&(p->last_eat_mutex.m));
+	// p->last_eat = gettimestamp();
+	// pthread_mutex_unlock(&(p->last_eat_mutex.m));
 	print_eat(p);
 	pthread_mutex_lock(&(p->remaining_eat_mutex.m));
 	p->remaining_eat--;
@@ -32,9 +32,9 @@ static void	eat(t_philosopher *p)
 	pthread_mutex_unlock(&(p->remaining_eat_mutex.m));
 	usleep(datas->tte * 1000);
 	// print_text(p, "finished eating");
-	// pthread_mutex_lock(&(p->last_eat_mutex.m));
-	// p->last_eat = gettimestamp();
-	// pthread_mutex_unlock(&(p->last_eat_mutex.m));
+	pthread_mutex_lock(&(p->last_eat_mutex.m));
+	p->last_eat = gettimestamp();
+	pthread_mutex_unlock(&(p->last_eat_mutex.m));
 }
 
 void	philosopher_eat(t_philosopher *p)
